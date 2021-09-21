fd624a323ed5d44013bdbc91052b080166033dcf
Marcos Macedo <marcosmacedoo.dev@gmail.com>
2021-09-21 16:09:23 -0300

    refactor: desacomplando app do server.js

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,9 @@
-import express from 'express'
-import cors from 'cors'
 import dotenv from 'dotenv'
-
-import { routes } from './routes'
+import { app } from './app'
 
 dotenv.config()
 
-const app = express()
 const port = 8000
-
-app.use(cors())
-app.use(express.json())
-app.use(routes)
 
 app.listen(process.env.PORT || port, () =>
     console.log(`Server running in http://localhost:${port}`)
